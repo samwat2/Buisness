58d5fa54b661efc3b40fd1f9b84225da3cf069b0
Sammy Watchman <swsw872@gmail.com>
2018-02-12 14:35:41 -0700

    fixes #1 fixes #2 fixes #3

--- main.rb
+++ main.rb
@@ -1,4 +1,5 @@
 require 'sinatra'
+require 'pony'
 
 get '/' do
 	erb :home
@@ -13,4 +14,13 @@ get '/features' do
 end
 get '/contact' do  
 	erb :contact
+end
+post '/contact-us' do
+	@first_name = params[:first_name]
+	@last_name = params[:last_name]
+	@body = params[:body]
+	@email_address = params[:email_adress]
+	@subject = params[:subject]
+	Pony.mail(:to => 'swsw872@gmail.com', :from => @email_adress, :subject => @subject, :body => @body)
+	erb :contacted
 end
