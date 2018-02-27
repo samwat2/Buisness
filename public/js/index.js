c8a551c0a1b94239e741f6d70e9e8487c2916407
Sammy Watchman <swsw872@gmail.com>
2018-02-27 12:05:54 -0700

    final

--- public/js/index.js
+++ public/js/index.js
@@ -8,6 +8,22 @@ $(function(){
 	    'margin' : '3% 0'
 		});
 	});
+	// $('#ajax').click(function(){
+	// 	$.get('/ajax-json', function(response){
+	// 		console.log(JSON.parse(response));
+	// 	});
+	// });
+	// $('#ajax-html').click(function(){
+	// 	$.get('/ajax-html', function(html){
+	// 		console.log(html);
+	// 		$('#thank-you').html(html);
+	// 	});
+	// });
+	// $('#descriptions').click(function(){
+	// 	$.get('/features', function(html){
+	// 		console.log(html);
+	// 		$('#place').html(html);
+	// 	});
+	// });
 });
-
 
